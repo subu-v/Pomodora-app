539b0cc5bb4f196c0b4978953194d8342904ff16
Subramani V <106590803+subu-v@users.noreply.github.com>
2023-08-09 16:27:40 +0530

    Apart from the actual timer functionality, the timer text, progress bar styes are done.
    The progress is yet to rotate in accordance with the timer set, just the styles for it are done.

--- src/UI/Timer.js
+++ src/UI/Timer.js
@@ -2,16 +2,15 @@ import { React, useState } from "react";
 import style from "./Timer.module.scss";
 
 const Timer = () => {
-  const [time, setTime] = useState("00:00");
-
   return (
     <div className={style["timer"]}>
+      <div className={style["timer-outer"]}></div>
       <div className={style["timer-inner"]}>
         <div className={style["timer-progress-bar"]}></div>
-        <div className={style["timer-counter-box"]}>
-          <p className={style["timer-counter"]}>{time}</p>
-          <button className={style["timer-counter-button"]}>PAUSE</button>
-        </div>
+      </div>
+      <div className={style["timer-counter"]}>
+        <p className={style["timer-counter-desc"]}>00:00</p>
+        <button className={style["timer-counter-btn"]}>RESTART</button>
       </div>
     </div>
   );

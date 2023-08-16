04e763c609017bc40873c01fbd79f50aca807b2e
Subramani V <106590803+subu-v@users.noreply.github.com>
2023-08-16 22:25:33 +0530

    progress bar nearly done.
    just need to implement smoothness for the progress bar and the color orange is slightly visible.

--- src/UI/Timer.js
+++ src/UI/Timer.js
@@ -3,20 +3,35 @@ import style from "./Timer.module.scss";
 
 const Timer = () => {
   const startTime = 1;
+  const startTimeInMSecs = startTime * 60 * 1000;
   const totalTimeInSecs = useRef(startTime * 60);
   const startStopTimerBtn = useRef(null);
   const startPomodoraInterval = useRef(null);
+  const progressOuterCircle = useRef(null);
   const [startStopTimerFlag, setstartStopTimerFlag] = useState(false);
 
   const timeCalculation = () => {
+    const progressBars = document.querySelectorAll(
+      `.${style["timer-progress-bar"]}`
+    );
     if (totalTimeInSecs.current === 0) {
       clearInterval(startPomodoraInterval.current);
       startStopTimerBtn.current.textContent = "RESTART";
     }
-    
+
     const minutes = Math.floor(totalTimeInSecs.current / 60);
     const seconds = totalTimeInSecs.current % 60;
 
+    const angle = ((totalTimeInSecs.current * 1000) / startTimeInMSecs) * 360;
+
+    if (angle > 180) {
+      // progressBars[1].style.transform = `rotate(180deg)`;
+      progressBars[0].style.rotate = `${angle}deg`;
+    } else {
+      progressBars[2].style.zIndex = 3;
+      progressBars[0].style.rotate = `${angle}deg`;
+      progressBars[1].style.rotate = `${angle}deg`;
+    }
     document.querySelector(
       `.${style["timer-countdown-desc"]}`
     ).textContent = `${minutes < 10 ? "0" + minutes : minutes}:${
@@ -40,15 +55,19 @@ const Timer = () => {
   };
 
   useEffect(() => {
-    if (startStopTimerFlag)
+    if (startStopTimerFlag) {
       startPomodoraInterval.current = setInterval(timeCalculation, 1000);
+      // progressOuterCircle.current.style.zIndex = 3;
+    }
     return () => clearInterval(startPomodoraInterval.current);
   }, [startStopTimerFlag]);
 
   return (
     <div className={style["timer"]}>
       <div className={style["timer-outer"]}></div>
-      <div className={style["timer-inner"]}>
+      <div ref={progressOuterCircle} className={style["timer-inner"]}>
+        <div className={style["timer-progress-bar"]}></div>
+        <div className={style["timer-progress-bar"]}></div>
         <div className={style["timer-progress-bar"]}></div>
       </div>
       <div className={style["timer-countdown"]}>
